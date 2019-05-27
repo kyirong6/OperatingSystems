047d8839d926e4355605a3f0e974295970901483
ckyirong <ckyirong@csil-cpu1.cs.surrey.sfu.ca>
2019-05-27 11:04:20 -0700

    added list.c

--- Assignments/A1/list/list.c
+++ Assignments/A1/list/list.c
@@ -4,9 +4,27 @@
 typedef struct nodeStruct {
     int item;
     struct nodeStruct *next;
-} node;
+} Node;
 
 
+Node* List_createNode(int item) {
+  Node* node;
+  node = (Node*) malloc(sizeof(Node));
+
+  node->item = item;
+  node->next = NULL;
+  return node;
+}
+
+void List_insertHead(Node **headRef, Node node) {
+  node->next = (*headRef);
+  (*headRef) = node;
+}
+
+void List_insertTail(Node **headRef, Node node) {
+  
+
+}
 
 
 
