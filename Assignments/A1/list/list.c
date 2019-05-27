b38d901d783df01ab0a904caf96ba1e9a4b394c8
ckyirong <ckyirong@csil-cpu1.cs.surrey.sfu.ca>
2019-05-27 14:14:52 -0700

    more for part2

--- Assignments/A1/list/list.c
+++ Assignments/A1/list/list.c
@@ -10,11 +10,13 @@ Node* List_createNode(int item) {
   return node;
 }
 
+
 void List_insertHead(Node **headRef, Node* node) {
-  node->next = (*headRef);
-  (*headRef) = node;
+  node->next = *headRef;
+  *headRef = node;
   return;
 }
+
 
 void List_insertTail(Node **headRef, Node* node) {
   node->next = NULL;
@@ -34,5 +36,61 @@ void List_insertTail(Node **headRef, Node* node) {
 }
 
 
+int List_countNodes(Node *head) {
+  if (head == NULL) {
+    return 0;
+  }
+
+  int count = 1;
+  Node* curr = head;
+  while (curr->next != NULL) {
+    curr = curr->next;
+    count++;
+  }
+  
+  return count;
+}
 
 
+Node* List_findNode(Node* head, int item) {
+  if (head == NULL) {
+    return NULL;
+  }
+
+  Node* curr = head;
+  while ((curr->item != item) && (curr->next != NULL)) {
+    curr = curr->next;
+  }
+
+  if (curr->item == item){
+    return curr;
+  } else {
+    return NULL;
+  }
+}
+
+
+void List_deleteNode(Node **headRef, Node* node) {
+  Node* head = *headRef;
+  
+  if (head->next == NULL) {
+    *headRef = NULL;
+    free(head);
+    return;
+  }
+
+  if (head->item == node->item) {
+    *headRef = head->next;
+    free(head);
+    return;
+  }
+
+  while (head->next->item != node->item) {
+    head = head->next;
+  }
+
+  Node* temp = head->next;
+  head->next = temp->next;
+  free(temp);
+  return;
+}
